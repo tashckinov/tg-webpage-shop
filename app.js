8caae3abebdc48d14e67e41485ecdb5b174a5f25
max <@max>
2023-04-03 23:03:30 +0300

    feat: babushka

---
+++ app.js
@@ -0,0 +1,36 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.textColor = "#2cab37";
+
+let item = "";
+
+const buttonItems = document.querySelectorAll('.btn');
+
+for (let buttonItem of buttonItems) {
+    buttonItem.addEventListener('click', function () {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            item = buttonItem.getAttribute("data-id")
+            tg.MainButton.setText(`Вы выбрали товар ${item}`);
+
+            tg.MainButton.show();
+        }
+    });
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    Telegram.sendData(item)
+})
+
+let usercard = document.getElementById("usercard")
+
+let p = document.createElement("p")
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`
+
+usercard.appendChild(p)
